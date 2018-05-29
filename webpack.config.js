4ca079048c069f5f447f62c757503e4366867ee7
Hyeonjong <nianelo4@gmail.com>
2018-05-30 01:17:24 +0900

    Apply clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -54,6 +55,7 @@ module.exports = {
 		]
 	},
 	plugins: [
+		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
 			title: 'Custom Template',
 			template: '_src/assets/index.html'
