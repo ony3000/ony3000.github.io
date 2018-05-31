e43de91bd0fc52e53cfc487e90c750de2a4801da
Hyeonjong <nianelo4@gmail.com>
2018-05-31 22:44:30 +0900

    Apply webpack-manifest-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
 	entry: {
@@ -67,6 +68,7 @@ module.exports = {
 		new MiniCssExtractPlugin({
 			filename: '[name].css',
 			chunkFilename: '[id].css',
-		})
+		}),
+		new ManifestPlugin()
 	]
 };
