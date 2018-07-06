f1ae0b99d0b65a35684392c5fda8b4002ea3c7a7
Hyeonjong <nianelo4@gmail.com>
2018-07-07 01:03:58 +0900

    Clean up packages which is not related to webpack config

--- _src/components/App.vue
+++ _src/components/App.vue
@@ -19,11 +19,7 @@ export default {
 	},
 	methods: {
 		printNow() {
-			return import(/* webpackChunkName: "moment" */ 'moment').then(module => {
-				const moment = module.default;
-
-				console.log(moment().format('MMMM Do YYYY, h:mm:ss a [GMT]Z'));
-			}).catch(error => 'An error occurred while loading the component');
+			console.log(new Date());
 		},
 	},
 };
